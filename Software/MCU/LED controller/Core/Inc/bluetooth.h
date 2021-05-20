79172bed4c7217b86d9ea1c9293ca8b3ee84be24
Retroboy <naskin.iv@gmail.com>
2021-05-20 22:21:29 +0300

    refactor bt commands

--- Software/MCU/LED controller/Core/Inc/bluetooth.h
+++ Software/MCU/LED controller/Core/Inc/bluetooth.h
@@ -13,8 +13,14 @@ typedef struct
 	uint8_t rx_len;
 } bluetooth_t;
 
+typedef struct
+{
+	uint8_t red, green, blue;
+	float brightness;
+} latest_data_t;
 
 void BT_listen(UART_HandleTypeDef *huart1, bluetooth_t *bt);
 void bt_rx_parse(bluetooth_t *bt);
+uint8_t str_color_to_int(bluetooth_t *bt);
 
 #endif /*_BLUETOOTH_H_*/
--- Software/MCU/LED controller/Core/Src/bluetooth.c
+++ Software/MCU/LED controller/Core/Src/bluetooth.c
@@ -8,13 +8,22 @@ enum Current_color
 	Red, Green, Blue
 } current_color;
 
+
 extern float brightness_temp;
 
+/*
+	Enable UART interrupt upon every received byte
+*/
 void BT_listen(UART_HandleTypeDef *huart1, bluetooth_t *bt)
 {
 	HAL_UART_Receive_IT(huart1, (uint8_t*)bt->rx_data, 1);
 }
-
+/*
+	list of bluetooth commands:
+	"ON)" - turn on the leds
+	"OFF)" - turn of the leds
+	"x.x.x)" - set color; x - decimal code RGB[0 .. 255]. For example: "245.2.24)"
+*/
 void bt_rx_parse(bluetooth_t *bt)
 {
 	if(bt->rx_data[0] >= '0' && bt->rx_data[0] <= '9')
@@ -24,6 +33,7 @@ void bt_rx_parse(bluetooth_t *bt)
 	}
 	if(bt->rx_data[0] == 'N')
 	{
+		/*set last brightness value got from com port*/
 		color_rx.brightness = brightness_temp;
 		set_color(color_rx);
 	}
@@ -37,50 +47,53 @@ void bt_rx_parse(bluetooth_t *bt)
 		switch(current_color)
 		{
 			case Red:
-				for(uint8_t j = bt->rx_len; j != 0; j--)
-				{
-					if(j == 3)
-						color_rx.red += (bt->buff[bt->rx_len - j] - '0') * 100;
-					if(j == 2)
-						color_rx.red += (bt->buff[bt->rx_len - j] - '0') * 10;
-					if(j == 1)
-						color_rx.red += bt->buff[bt->rx_len - j] - '0';	
-				}
-				bt->rx_len = 0;
+				color_rx.red = str_color_to_int(bt);
 				current_color = Green;
 				break;
 				
 			case Green:
-				for(uint8_t j = bt->rx_len; j != 0; j--)
-				{
-					if(j == 3)
-						color_rx.green += (bt->buff[bt->rx_len - j] - '0') * 100;
-					if(j == 2)
-						color_rx.green += (bt->buff[bt->rx_len - j] - '0') * 10;
-					if(j == 1)
-						color_rx.green += bt->buff[bt->rx_len - j] - '0';	
-				}
-				bt->rx_len = 0;
+				color_rx.green = str_color_to_int(bt);
 				current_color = Blue;
 				break;
 				
 			case Blue:
-				for(uint8_t j = bt->rx_len; j != 0; j--)
-				{
-					if(j == 3)
-						color_rx.blue += (bt->buff[bt->rx_len - j] - '0') * 100;
-					if(j == 2)
-						color_rx.blue += (bt->buff[bt->rx_len - j] - '0') * 10;
-					if(j == 1)
-						color_rx.blue += bt->buff[bt->rx_len - j] - '0';	
-				}
+				color_rx.blue = str_color_to_int(bt);
 				set_color(color_rx);
-				color_rx.red = color_rx.green = color_rx.blue = 0;
-				bt->rx_len = 0;
 				current_color = Red;
 				break;
 		}
-	}			
+	}
+	/*listen next data byte*/
 	BT_listen(&huart1, bt);
 }
 
+
+uint8_t str_color_to_int(bluetooth_t *bt)
+{
+	uint8_t color_val = 0;
+	
+	for(uint8_t j = bt->rx_len; j != 0; j--)
+	{
+		if(bt->rx_len == 3)
+		{
+			if(j == 3)
+				color_val = (bt->buff[bt->rx_len - j] - '0') * 100;
+			if(j == 2)
+				color_val += (bt->buff[bt->rx_len - j] - '0') * 10;
+			if(j == 1)
+				color_val += bt->buff[bt->rx_len - j] - '0';
+		}
+		if(bt->rx_len == 2)
+		{
+			if(j == 2)
+				color_val = (bt->buff[bt->rx_len - j] - '0') * 10;
+			if(j == 1)
+				color_val += bt->buff[bt->rx_len - j] - '0';
+		}
+		if(bt->rx_len == 1)
+				color_val = bt->buff[bt->rx_len - j] - '0';
+	}
+	bt->rx_len = 0;
+	
+	return color_val;	
+}
